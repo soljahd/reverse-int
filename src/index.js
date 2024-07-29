f952bcc338e798f84cfdc47a39bb58573cab807d
soljah <moskuitoz@gmail.com>
2024-07-29 18:44:58 +0300

    feat: task solved

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    const num = Math.abs(n);
+    let numArr = Array.from(String(num), ((x) => Number(x)));
+    return (Number(numArr.reverse().join('')));
 }
